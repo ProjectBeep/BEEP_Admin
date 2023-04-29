f430527531502e3e7161138e56f1ac1a02858997
김명석 <audxo112@naver.com>
2023-04-29 23:05:32 +0900

    Retrofit 관련 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm")
     id("org.jetbrains.compose")
+    id("com.google.devtools.ksp").version("1.8.20-1.0.11")
 }
 
 group = "com.lighthouse.beep.admin"
@@ -18,6 +19,14 @@ kotlin {
 
 dependencies {
     implementation(compose.desktop.currentOs)
+
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+    implementation("com.squareup.moshi:moshi-adapters:1.14.0")
+
+    ksp("com.squareup.moshi:moshi-kotlin-codegen:1.14.0")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
 }
 
 compose.desktop {
