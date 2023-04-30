d1e2e52f2292350b6f0c7f70c030261c815e4510
김명석 <audxo112@naver.com>
2023-04-30 10:45:45 +0900

    Ktor 의존성으로 변경 및 gitIgnore 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     kotlin("jvm")
     id("org.jetbrains.compose")
-    id("com.google.devtools.ksp").version("1.8.20-1.0.11")
 }
 
 group = "com.lighthouse.beep.admin"
@@ -20,13 +19,10 @@ kotlin {
 dependencies {
     implementation(compose.desktop.currentOs)
 
-    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
-    implementation("com.squareup.moshi:moshi-adapters:1.14.0")
-
-    ksp("com.squareup.moshi:moshi-kotlin-codegen:1.14.0")
-
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("io.ktor:ktor-client-core:2.3.0")
+    implementation("io.ktor:ktor-client-json:2.3.0")
+    implementation("io.ktor:ktor-client-logging:2.3.0")
+    implementation("io.ktor:ktor-client-serialization:2.3.0")
 }
 
 compose.desktop {
