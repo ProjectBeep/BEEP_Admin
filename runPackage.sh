1a98835d98403187c5450a7fab87702fa4136149
김명석 <audxo112@naver.com>
2023-05-06 16:50:03 +0900

    os 에 따른 package 스크립트 제작

--- runPackage.sh
+++ runPackage.sh
@@ -1,28 +1,26 @@
+#!/bin/bash
+
 CHECK_OS="`uname -s`"
 
 if [[ "$CHECK_OS" = "Darwin"* ]]; then
-    # Do something under Mac OS X platform
     THIS_OS="MAC"
 elif [[ "$CHECK_OS" = "Linux"* ]]; then
-    # Do something under GNU/Linux platform
     THIS_OS="LINUX"
 elif [[ "$CHECK_OS" = "MINGW32"* ]]; then
-    # Do something under Windows NT platform
     THIS_OS="WIN"
 elif [[ "$CHECK_OS" = "MINGW64"* ]]; then
-    # Do something under Windows NT platform
     THIS_OS="WIN"
 elif [[ "$CHECK_OS" = "CYGWIN"* ]]; then
-    # Do something under Windows NT platform
     THIS_OS="WIN"
 fi
 
-if [[ "$THIS_OS" = "MAC"]]; then
-    ./gradlew packageDmg
+sudo chmod +x ./gradlew
+if [[ "$THIS_OS" = "MAC" ]]; then
+    sudo ./gradlew packageDmg
 elif [[ "$THIS_OS" = "LINUX" ]]; then
-    ./gradlew packageDeb
+    sudo ./gradlew packageDeb
 elif [[ "$THIS_OS" = "WIN" ]]; then
-    ./gradlew packageMsi
-if
+    sudo ./gradlew packageMsi
+fi
 
 echo "build/compose/binaries/main 폴더를 확인해주세요"
